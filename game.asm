5683915379938149d4b03d76248d8b31bdfe0669
half-blood-prince <121034510+half-blood-prince@users.noreply.github.com>
2023-04-06 12:35:49 +0530

    Update game.asm

--- game.asm
+++ game.asm
@@ -15,7 +15,7 @@ T4 DB 32, 32, 4, 32, 32, 32,  4, 32,  4,  4,  4,  4, 32,  4, 32, 32, 32, 4, 32, 
 ;--------------------------------------------------------------------
 
  
-TAGLINE DB  'Developed by Tanvir Ahmed Sojal$' ; DEVELOPER NAME 
+TAGLINE DB  'Developed by Yash Singh, Aman, Falguni, Deepak, Aaron$' ; DEVELOPER NAME 
 
 
 ; ----------------- IMPORTANT STRINGS USED THROUGHOUT THE GAME -----------------
